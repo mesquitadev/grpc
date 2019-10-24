05e59d8617254ae6bf3159ce9865c5a063555d90
Victor Mesquita <mesquitadev@gmail.com>
2019-10-24 13:52:08 -0300

    Script 1

--- VideoStream_fromServer_Sample/install.sh
+++ VideoStream_fromServer_Sample/install.sh
@@ -6,14 +6,27 @@ echo "#==========================================="
 
 echo "Atualizando Repositórios..."
 sudo apt update
+echo ""
+echo ""
+echo ""
+echo ""
 echo "Instalando Libraries..."
 sudo apt install libopencv-dev -y
 sudo apt install python-opencv -y
+sudo apt install python-pip -y
+echo ""
+echo ""
+echo ""
+echo ""
 echo "Criando venv"
 virtualenv venv
 echo "Ativando Venv..."
 cd venv
 bin/activate
+echo ""
+echo ""
+echo ""
+echo ""
 echo "Instalando libs Python3"
 sudo python3 -m pip install grpcio grpcio-tools scikit-video
 sudo python3 -m pip install -r requeriments.txt
