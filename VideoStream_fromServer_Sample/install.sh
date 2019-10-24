ffb774bbcb223ac71885538ebfbed7936e93ccf0
Victor Mesquita <mesquitadev@gmail.com>
2019-10-24 13:39:18 -0300

    Correção Script

--- VideoStream_fromServer_Sample/install.sh
+++ VideoStream_fromServer_Sample/install.sh
@@ -4,24 +4,11 @@ echo "==========================================="
 echo "\t EYETECH INSTALLER STREAMING SERVICE"
 echo "#==========================================="
 
-echo -n libopencv-dev python-opencv
-echo ""
-echo ""
-pacote=$(dpkg --get-selections | grep "$nome" )
-echo ""
-echo -n "Verificando se o Pacote $nome esta instalado"
-echo ""
-echo ""
-echo ""
-sleep 2
-if [ -n "$pacote" ] ;
-then echo
-     echo "Pacote $nome ja instalado"
-else echo
-     echo "Pacote $nome Necessario-> Nao instalado"
-     echo "Instalando Automaticamente Pacote Dialog..."
-     sudo apt update
-     sudo apt install libopencv-dev
-     sudo apt install python-opencv
-     sudo python3 -m pip install grpcio grpcio-tools scikit-video
-fi
+echo "Atualizando Repositórios..."
+sudo apt update
+echo "Instalando Libraries..."
+sudo apt install libopencv-dev
+sudo apt install python-opencv
+echo "Instalando libs Python3"
+sudo python3 -m pip install grpcio grpcio-tools scikit-video
+sudo python3 -m pip install requeriments.txt
